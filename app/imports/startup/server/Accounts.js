e27aa830bbfd395ef0b97a3bf4bbc4edbf706827
Michelle Leano <leanomc@hawaii.edu>
2023-01-24 23:46:51 -1000

    user profiles collection + default data

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -5,12 +5,12 @@ import { UserProfiles } from '../../api/user/UserProfileCollection';
 
 /* eslint-disable no-console */
 
-function createUser(email, role, firstName, lastName, password) {
-  console.log(`  Creating user ${email} with role ${role}.`);
+function createUser(email, role, firstName, lastName, password, position) {
+  console.log(`  Creating user ${email} with role ${role} and position ${position}.`);
   if (role === ROLE.ADMIN) {
     AdminProfiles.define({ email, firstName, lastName, password });
   } else { // everyone else is just a user.
-    UserProfiles.define({ email, firstName, lastName, password });
+    UserProfiles.define({ email, firstName, lastName, password, position });
   }
 }
 
@@ -18,7 +18,7 @@ function createUser(email, role, firstName, lastName, password) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName, lastName }) => createUser(email, role, firstName, lastName, password));
+    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName, lastName, position }) => createUser(email, role, firstName, lastName, password, position));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
