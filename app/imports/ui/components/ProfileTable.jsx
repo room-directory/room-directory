3d7d7d2abd6eb8601046a5ea6a83e4f0ba47a7a1
Michelle Leano <leanomc@hawaii.edu>
2023-02-13 22:51:32 -1000

    #100 delete works

--- app/imports/ui/components/ProfileTable.jsx
+++ app/imports/ui/components/ProfileTable.jsx
@@ -1,110 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, Container, Form, FormControl, Row, Table } from 'react-bootstrap';
+import swal from 'sweetalert';
+import { Card, Col, Row, Button } from 'react-bootstrap';
+import { removeItMethod } from '../../api/base/BaseCollection.methods';
+import { UserProfiles } from '../../api/user/UserProfileCollection';
 
-const ProfileTable = () => {
-  const [filterText, setFilterText] = useState('');
-  const [data, setData] = useState([
-    { email: 'admin@foo.com',
-      firstName: 'Ad',
-      lastName: 'Min',
-      position: 'faculty' },
-    { email: 'john@foo.com',
-      firstName: 'John',
-      lastName: 'Foo',
-      position: 'office' },
-  ]);
+const ProfileTable = ({ account, eventKey }) => {
 
-  const handleFilterTextChange = (event) => {
-    setFilterText(event.target.value);
-  };
-
-  const addRow = () => {
-    setData([
-      ...data,
-      { email: 'New email',
-        firstName: 'New Name',
-        lastName: 'New Name',
-        position: 'Position' },
-    ]);
-  };
-
-  const editRow = (index) => {
-    const newData = [...data];
-    newData[index].firstName = 'Edited Name';
-    setData(newData);
-  };
-
-  const deleteRow = (index) => {
-    const newData = [...data];
-    newData.splice(index, 1);
-    setData(newData);
+  const del = () => {
+    const collectionName = UserProfiles.getCollectionName();
+    const instance = account._id;
+    swal({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      dangerMode: true,
+      buttons: true,
+    }).then((result) => {
+      if (result) {
+        removeItMethod.callPromise({ collectionName, instance })
+          .catch(error => swal('Error', error.message, 'error'))
+          .then(() => swal('Profile has been deleted!', {
+            icon: 'success',
+          }));
+      } else {
+        swal('Profile is safe!');
+      }
+    });
   };
 
   return (
-    <Container>
-      <Row>
-        <Col xs={6}>
-          <Form inline className="mb-3">
-            <FormControl
-              type="text"
-              placeholder="Filter by name..."
-              value={filterText}
-              onChange={handleFilterTextChange}
-            />
-          </Form>
-        </Col>
-        <Col xs={6} className="d-flex justify-content-end">
-          <div className="text-right">
-            <Button variant="success" onClick={addRow}>
-              + Add
-            </Button>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Last Name</th>
-              <th>First Name</th>
-              <th>Email</th>
-              <th>Position</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.filter((row) => row.lastName.toLowerCase().includes(filterText.toLowerCase())).map((row, index) => (
-              <tr key={row.email}>
-                <td>{row.lastName}</td>
-                <td>{row.firstName}</td>
-                <td>{row.email}</td>
-                <td>{row.position}</td>
-                <td>
-                  <Button variant="primary" onClick={() => editRow(index)}>
-                    Edit
-                  </Button>{' '}
-                  <Button variant="danger" onClick={() => deleteRow(index)}>
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Row>
-    </Container>
+    <Card style={{ border: 'none', borderRadius: 0 }}>
+      <Card.Header style={eventKey % 2 === 0 ? { backgroundColor: 'whitesmoke', border: 'none' } : { backgroundColor: '#fbfbfb', border: 'none' }}>
+        <Row>
+          <Col>{`${account.lastName}`}</Col>
+          <Col>{`${account.firstName}`}</Col>
+          <Col>{account.email}</Col>
+          <Col>{account.position}</Col>
+          <Col xs={1}><Button variant="primary">Edit</Button></Col>
+          <Col xs={1}><Button variant="danger" onClick={del}>Delete</Button></Col>
+        </Row>
+      </Card.Header>
+    </Card>
   );
 };
 
 /* Referencing the Base Collection */
 ProfileTable.propTypes = {
-  profile: PropTypes.shape({
-    email: PropTypes.string,
+  account: PropTypes.shape({
     firstName: PropTypes.string,
     lastName: PropTypes.string,
+    email: PropTypes.string,
     position: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
+  eventKey: PropTypes.string.isRequired,
 };
 
 export default ProfileTable;
