5d7ed0acd173f922bca1d35e13b58fb96a6526f2
jing2003 <yingying.he2003@gmail.com>
2023-02-22 11:32:06 -1000

    Update Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -7,7 +7,7 @@ import { Github } from 'react-bootstrap-icons';
 const Footer = () => {
   const divStyle = { paddingTop: '15px' };
   const white = { color: 'white' };
-  const green = { color: '#B2D3C2' };
+  const green = { color: '#99EDC3' };
   return (
     <footer className="mt-auto bg-light text-center">
       <Container style={divStyle}>
@@ -17,13 +17,13 @@ const Footer = () => {
               <Image src="/images/uh-logo.jpg" roundedCircle style={{ width: '200px' }} />
             </Col>
             <Col>
-              <h4 style={white}>About Us</h4>
+              <h4 style={green}>About Us</h4>
               <hr style={white} />
               <p style={green}>The goal of Room Directory is to create user friendly interface for office space management of the ICS department</p>
               <a href="https://room-directory.github.io/" className="text-white">Room Directory Home Page</a>
             </Col>
             <Col>
-              <h4 style={white}>Useful Links</h4>
+              <h4 style={green}>Useful Links</h4>
               <hr style={white} />
               <NavLink to="/" className="text-white">Home</NavLink>
               <br />
@@ -36,7 +36,7 @@ const Footer = () => {
               <NavLink to="/club" className="text-white">Club Information</NavLink>
             </Col>
             <Col>
-              <h4 style={white}>Contact Us</h4>
+              <h4 style={green}>Contact Us</h4>
               <hr style={white} />
               <a href="https://github.com/room-directory/room-directory" className="text-white"> <Github size={30} /> </a>
             </Col>
