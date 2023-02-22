eb7bc3bb66cd9446809871a9d5be0093f52d45b2
jing2003 <yingying.he2003@gmail.com>
2023-02-22 11:10:41 -1000

    Update Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import { Container, Col, Row, Image } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { Github } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => {
   const divStyle = { paddingTop: '15px' };
+  const white = { color: 'white' };
+  const green = { color: '#B2D3C2' };
   return (
     <footer className="mt-auto bg-light text-center">
       <Container style={divStyle}>
         <Col>
           <Row>
             <Col>
-              <Image src="/images/uh-logo.jpg" roundedCircle />
+              <Image src="/images/uh-logo.jpg" roundedCircle style={{ width: '200px' }} />
             </Col>
             <Col>
+              <h4 style={white}>About Us</h4>
+              <hr style={white} />
+              <p style={green}>The goal of Room Directory is to create user friendly interface for office space management of the ICS department</p>
               <a href="https://room-directory.github.io/" className="text-white">Room Directory Home Page</a>
             </Col>
             <Col>
-              <a href="https://github.com/room-directory/room-directory" className="text-white">Contact Us</a>
+              <h4 style={white}>Useful Links</h4>
+              <hr style={white} />
+              <NavLink to="/" className="text-white">Home</NavLink>
+              <br />
+              <NavLink to="/signin" className="text-white">Sign In</NavLink>
+              <br />
+              <NavLink to="/signup" className="text-white">Sign Up</NavLink>
+              <br />
+              <NavLink to="/faculty" className="text-white">Faculty Information</NavLink>
+              <br />
+              <NavLink to="/club" className="text-white">Club Information</NavLink>
             </Col>
             <Col>
-              <NavLink to="/signup" className="text-white">Register</NavLink>
+              <h4 style={white}>Contact Us</h4>
+              <hr style={white} />
+              <a href="https://github.com/room-directory/room-directory" className="text-white"> <Github size={30} /> </a>
             </Col>
           </Row>
           <br />
