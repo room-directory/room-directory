d7175faaf163cdc0fa94014b0cace467910d8cf4
Jeraldine Milla <jeraldinemilla@gmail.com>
2023-02-28 11:18:07 -1000

    Typewriting for landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import TypeWriterEffect from 'react-typewriter-effect';
 import { PAGE_IDS } from '../utilities/PageIDs';
-import Slideshow from '../components/Slideshow';
 
 /* A simple static component to render some text for the landing page. */
+
 const Landing = () => (
   <Container
     fluid
     id={PAGE_IDS.LANDING}
     className="p-0 m-0"
     style={{
-      backgroundImage: "url('/images/post.jpg')",
+      height: '65vh',
+      backgroundImage: "url('../images/post.png')",
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }}
   >
+    <Row style={{ marginTop: '200px', marginBottom: '80%' }}>
+      <Col className="d-flex justify-content-start align-items-center" md={{ span: 4, offset: 2 }}>
+        <div className="text-center text-white text-opacity-100 cont">
+          <TypeWriterEffect
+            startDelay={200}
+            cursorColor="#3F3D56"
+            multiText={[
+              'Creating a user friendly interface for office space management of the ICS department',
+            ]}
+            multiTextDelay={100}
+            typeSpeed={50}
+          />
+        </div>
+      </Col>
 
-    <Row>
-      {/*  <Slideshow /> */}
-      {/* </Row> */}
-      {/* <Row className="text-center h-50" id="instruction"> */}
-      {/*  <Col> */}
-      {/*    Room directory is cool and useful. */}
-      {/*  </Col> */}
-      {/*  <Col> */}
-      {/*    Click on buttons to do things on website. */}
-      {/*  </Col> */}
     </Row>
-    {/* <div /> */}
+
   </Container>
 );
 
