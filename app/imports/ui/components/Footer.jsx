9f57d3287f329783d1c041b69d8f0c62ca757c0f
Jeraldine Milla <jeraldinemilla@gmail.com>
2023-03-06 08:58:36 -1000

    update on Landing page and footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -6,34 +6,35 @@ import { Github } from 'react-bootstrap-icons';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => {
   const divStyle = { paddingTop: '15px' };
-  const white = { color: 'white' };
-  const green = { color: '#99EDC3' };
+  const white = { color: 'white', fontSize: '13px' };
+  const green = { color: '#99EDC3', fontSize: '16px' };
+  const line = { color: 'white' };
   return (
     <footer className="mt-auto bg-light text-center">
       <Container style={divStyle}>
         <Col>
           <Row>
-            <Col>
-              <Image src="/images/uh-logo.jpg" roundedCircle style={{ width: '135px' }} />
+            <Col style={{ paddingTop: '20px' }}>
+              <Image src="/images/uh-logo.jpg" roundedCircle style={{ width: '80px' }} />
             </Col>
             <Col>
-              <h4 style={green}>About Us</h4>
-              <hr style={white} />
-              <p style={green}>The goal of Room Directory is to create user friendly interface for office space management of the ICS department</p>
+              <h6 style={green}>About Us</h6>
+              <hr style={line} />
+              <p style={white}>The goal of Room Directory is to create user friendly interface for office space management of the ICS department</p>
             </Col>
             <Col>
-              <h4 style={green}>Useful Links</h4>
-              <hr style={white} />
-              <a href="https://room-directory.github.io/" className="text-white">Room Directory Home Page</a>
+              <h6 style={green}>Useful Links</h6>
+              <hr style={line} />
+              <a href="https://room-directory.github.io/" className="text-white" style={white}>Room Directory Home Page</a>
               <br />
-              <NavLink to="/faculty" className="text-white">Faculty Information</NavLink>
+              <NavLink to="/faculty" className="text-white" style={white}>Faculty Information</NavLink>
               <br />
-              <NavLink to="/club" className="text-white">Club Information</NavLink>
+              <NavLink to="/club" className="text-white" style={white}>Club Information</NavLink>
             </Col>
             <Col>
-              <h4 style={green}>Contact Us</h4>
-              <hr style={white} />
-              <a href="https://github.com/room-directory/room-directory" className="text-white"> <Github size={30} /> </a>
+              <h6 style={green}>Contact Us</h6>
+              <hr style={line} />
+              <a href="https://github.com/room-directory/room-directory" className="text-white" style={white}> <Github size={30} /> </a>
             </Col>
           </Row>
         </Col>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,40 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import TypeWriterEffect from 'react-typewriter-effect';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
+const sentences = [
+  'Creating a user friendly interface for office space management of the ICS department.',
+  'Welcome to the ICS Room Directory!',
+];
+const Landing = () => {
+  const [currentSentence, setCurrentSentence] = useState('');
+  const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-const Landing = () => (
-  <Container
-    fluid
-    id={PAGE_IDS.LANDING}
-    className="p-0 m-0"
-    style={{
-      height: '65vh',
-      backgroundImage: "url('../images/post.png')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}
-  >
-    <Row style={{ marginTop: '200px', marginBottom: '80%' }}>
-      <Col className="d-flex justify-content-start align-items-center" md={{ span: 4, offset: 2 }}>
-        <div className="text-center text-white text-opacity-100 cont">
-          <TypeWriterEffect
-            startDelay={200}
-            cursorColor="#3F3D56"
-            multiText={[
-              'Creating a user friendly interface for office space management of the ICS department',
-            ]}
-            multiTextDelay={100}
-            typeSpeed={50}
-          />
-        </div>
-      </Col>
-
-    </Row>
-
-  </Container>
-);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (currentIndex < sentences[currentSentenceIndex].length) {
+        const next = sentences[currentSentenceIndex][currentIndex];
+        setCurrentSentence((prev) => prev + next);
+        setCurrentIndex((prev) => prev + 1);
+      } else {
+        setCurrentIndex(0);
+        setCurrentSentence('');
+        setCurrentSentenceIndex((prev) => (prev === sentences.length - 1 ? 0 : prev + 1));
+      }
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, [currentIndex, currentSentenceIndex]);
+  return (
+    <Container
+      fluid
+      id={PAGE_IDS.LANDING}
+      className="p-0 m-0"
+      style={{
+        backgroundImage: "url('../images/post.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundClip: 'padding-box',
+        maxHeight: '565px',
+      }}
+    >
+      <Row style={{ marginTop: '200px', marginBottom: '90%' }}>
+        <Col className="d-flex justify-content-start align-items-center" md={{ span: 4, offset: 1 }}>
+          <div className="text-center text-white text-opacity-100 cont">
+            <p>{currentSentence}</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Landing;
