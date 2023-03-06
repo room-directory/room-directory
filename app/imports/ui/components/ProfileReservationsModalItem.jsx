b99b2cf89a4760274dbc34109285ae3ee49f931a
khirano7 <khirano7@hawaii.edu>
2023-03-06 12:46:31 -1000

    updated profile UI and added room assignments (faculty only)

--- app/imports/ui/components/ProfileReservationsModalItem.jsx
+++ app/imports/ui/components/ProfileReservationsModalItem.jsx
@@ -2,21 +2,8 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-/* function to convert date */
-function dateToTime(date) {
-  const cDay = date.getDate();
-  const cMonth = date.getMonth() + 1;
-  const cYear = date.getFullYear();
-  const cHour = date.getUTCHours();
-  let cMin = date.getUTCMinutes();
-  if (cMin < 10) { cMin = `0${cMin}`; }
-  const ampm = cHour >= 12 ? 'pm' : 'am';
-
-  return `${cMonth}/${cDay}/${cYear} ${cHour % 12}:${cMin} ${ampm}`;
-}
-
 /* Reservation item for the profile modal */
-const ProfileReservationsModalItem = ({ reservation }) => (
+const ProfileReservationsModalItem = ({ reservation, dateToTime }) => (
   <Col>
     Name: {reservation.firstName} {reservation.lastName} <br />
     Room: #{reservation.roomNumber} <br />
@@ -34,6 +21,7 @@ ProfileReservationsModalItem.propTypes = {
     endTime: PropTypes.instanceOf(Date),
     _id: PropTypes.string,
   }).isRequired,
+  dateToTime: PropTypes.func.isRequired,
 };
 
 export default ProfileReservationsModalItem;
