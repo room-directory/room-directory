ce8e7402e0ac574522c9244dcb5bccad4d497bca
timothyro <timr808@gmail.com>
2023-04-06 14:09:52 -1000

    add sort to room list page

--- app/imports/ui/pages/RoomList.jsx
+++ app/imports/ui/pages/RoomList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Dropdown, DropdownButton, ButtonGroup, Table, Button } from 'react-bootstrap';
+import { Container, Row, Col, Dropdown, DropdownButton, ButtonGroup, Table, Button } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Room } from '../../api/room/RoomCollection';
 import { PAGE_IDS } from '../utilities/PageIDs';
@@ -28,33 +28,47 @@ const RoomList = () => {
       ready: rdy,
     };
   }, []);
+  let roomList = rooms;
   const [hoverRoom, setHoverRoom] = useState('default');
   const [scale, setScale] = useState(1);
+  const [sortingBy, setSortingBy] = useState('roomNumber');
+  const [category, setCategory] = useState('Room Number');
+  roomList.sort(function (a, b) {
+    return a[sortingBy].localeCompare(b[sortingBy]);
+  });
   const changeScale = () => {
     setScale(scale + 1);
     setScale((scale % 2) + 1);
   };
   return (ready ? (
     <Container id={PAGE_IDS.ROOM_LIST} className="py-3 overflow-hidden">
-      <Row>
-        <ButtonGroup aria-label="Filter group">
-          <DropdownButton variant="primary" title="Building" className="sharp me-3">
-            <Dropdown.Item href="#/action-1">POST</Dropdown.Item>
-          </DropdownButton>
-          <DropdownButton variant="primary" title="Floor" className=" sharp me-3">
-            <Dropdown.Item href="#/action-1">3</Dropdown.Item>
-          </DropdownButton>
-        </ButtonGroup>
-        <Row>
-          <ButtonGroup aria-label="Zoom group">
-            <div className="mt-2">
-              <Button className="btn btn-primary" onClick={() => changeScale()}>{scale === 1 ? 'Zoom in' : 'Zoom out'}</Button>
-            </div>
+      <Row style={{ textAlign: 'center' }}>
+        <Col>
+          <ButtonGroup aria-label="Filter group">
+            <DropdownButton variant="primary" title="Building" className="sharp me-3">
+              <Dropdown.Item href="#/action-1">POST</Dropdown.Item>
+            </DropdownButton>
+            <DropdownButton variant="primary" title="Floor" className=" sharp me-3">
+              <Dropdown.Item href="#/action-1">3</Dropdown.Item>
+            </DropdownButton>
           </ButtonGroup>
-
-        </Row>
+          <Row>
+            <ButtonGroup aria-label="Zoom group" style={{ justifyContent: 'center' }}>
+              <div className="mt-2">
+                <Button className="btn btn-primary" onClick={() => changeScale()}>{scale === 1 ? 'Zoom in' : 'Zoom out'}</Button>
+              </div>
+            </ButtonGroup>
+          </Row>
+        </Col>
+        <Col />
+        <Col>
+          <DropdownButton variant="primary" title={`Sort by: ${category}`} className="sharp me-3">
+            <Dropdown.Item href="#/action-1" onClick={() => { setSortingBy('roomNumber'); setCategory('Room Number'); }}>Room Number</Dropdown.Item>
+            <Dropdown.Item href="#/action-1" onClick={() => { setSortingBy('type'); setCategory('Room Type'); }}>Room Type</Dropdown.Item>
+          </DropdownButton>
+        </Col>
       </Row>
-      <Row className="d-flex w-auto h-auto flex-nowrap">
+      <Row className="d-flex w-auto h-auto flex-nowrap justify-content-center">
         <div className="map-container" style={{ width: 870 }}>
           <SvgComponent rooms={rooms} hoverRoom={hoverRoom} scale={scale} facultyCollection={faculty} />
         </div>
@@ -67,7 +81,7 @@ const RoomList = () => {
               </tr>
             </thead>
             <tbody>
-              {rooms.map((room) => <RoomListTableRow room={room} hoverRoom={hoverRoom} setHoverRoom={setHoverRoom} key={room._id} />)}
+              {roomList.map((room) => <RoomListTableRow room={room} hoverRoom={hoverRoom} setHoverRoom={setHoverRoom} key={room._id} />)}
             </tbody>
           </Table>
         </div>
