2b475df2e1ff711c9ddd1294e3f86b531d19e590
jing2003 <yingying.he2003@gmail.com>
2023-04-17 11:25:24 -1000

    delete reservation from mongo

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,16 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { Reservations } from '../../api/reservation/ReservationCollection';
 import { Room } from '../../api/room/RoomCollection';
 import { FacultyProfiles } from '../../api/faculty/FacultyProfileCollection';
 import { RoomResources } from '../../api/room/RoomResourceCollection';
 import { Club } from '../../api/club/ClubCollection';
 /* eslint-disable no-console */
-
-// Initialize the database with a default reservation data document.
-function addReservationData(data) {
-  console.log(`  Adding reservation for (${data.email})`);
-  Reservations.define(data);
-}
 
 // Initialize the database with a default room data document.
 function addRoomData(data) {
@@ -34,14 +27,6 @@ function addRoomResourceData(data) {
 function addClubData(data) {
   console.log(`  Adding: (${data.clubName})`);
   Club.define(data);
-}
-
-// Initialize the ReservationCollection if empty.
-if (Reservations.count() === 0) {
-  if (Meteor.settings.defaultReservationData) {
-    console.log('Creating default reservations data.');
-    Meteor.settings.defaultReservationData.map(data => addReservationData(data));
-  }
 }
 
 // Initialize the RoomCollection if empty.
