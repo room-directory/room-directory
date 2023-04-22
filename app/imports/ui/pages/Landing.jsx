812247a62b0a3bb261df6d8334284bbb03290600
Jeraldine Milla <jeraldinemilla@gmail.com>
2023-04-22 07:35:52 -1000

    updated landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useRef, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
 
 const Landing = () => {
   const [currentSentence, setCurrentSentence] = useState('');
+  const cardRef = useRef(null);
 
   useEffect(() => {
-    const sentence = 'Welcome to the ICS Room Directory! \n Creating a user friendly interface for office space management of the ICS department.';
+    const sentence = 'Welcome to the ICS Room Directory! \n' +
+        ' Creating a user friendly interface for office space management of the ICS department.';
     let currentIndex = 0;
     let timeoutId = null;
 
@@ -27,6 +29,14 @@ const Landing = () => {
 
     return () => clearTimeout(timeoutId);
   }, []);
+  useEffect(() => {
+    if (cardRef.current) {
+      const { width, height } = cardRef.current.getBoundingClientRect();
+      cardRef.current.style.width = `${width}px`;
+      cardRef.current.style.height = `${height}px`;
+      cardRef.current.style.fontSize = `${height * 0.5}px`; // Change this value to adjust the font size
+    }
+  }, [currentSentence]);
 
   // const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
   // const [currentIndex, setCurrentIndex] = useState(0);
@@ -68,11 +78,13 @@ const Landing = () => {
         maxHeight: '565px',
       }}
     >
-      <Row style={{ marginTop: '200px', marginBottom: '90%' }}>
-        <Col className="d-flex justify-content-start align-items-center" md={{ span: 4, offset: 1 }}>
-          <div className="text-center text-white text-opacity-100 cont">
-            <p>{currentSentence}</p>
-          </div>
+      <Row style={{ marginTop: '150px', marginBottom: '40%' }}>
+        <Col className="d-flex justify-content-start align-items-center">
+          {/* md={{ span: 6, offset: 3 }} <div className="text-center text-white text-opacity-100 cont"> */}
+          <Card ref={cardRef} className="p-5 text-center land-card" style={{ backgroundColor: 'rgba(0, 63, 24, 0.70)', position: 'relative' }}>
+            <p className="text-white cont">{currentSentence}</p>
+          </Card>
+          {/* </div> */}
         </Col>
       </Row>
     </Container>
