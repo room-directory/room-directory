3604b764c819e9705dce5f9022595799bb990ba7
KyoufuuN1K1 <54867136+KyoufuuN1K1@users.noreply.github.com>
2023-04-24 10:50:05 -1000

    Attempted to add keys to map in faculty-related pages

--- app/imports/ui/components/Faculty.jsx
+++ app/imports/ui/components/Faculty.jsx
@@ -5,15 +5,15 @@ import PropTypes from 'prop-types';
 const Faculty = ({ faculty, user }) => (
   <tr>
     <td><img alt="" src={faculty.image} width="180" height="180" /></td>
-    <td>{`${faculty.firstName} ${faculty.lastName}`}<br />{faculty.role.map((role, n) => <div key={n}>{role}</div>)}</td>
-    <td>{faculty.email} {faculty.phone.map((phone) => <div>{phone}</div>)}</td>
+    <td>{`${faculty.firstName} ${faculty.lastName}`}<br />{faculty.role.map((role) => <div key={role}>{role}</div>)}</td>
+    <td>{faculty.email} {faculty.phone.map((phone) => <div key={phone}>{phone}</div>)}</td>
     { faculty.officeLocation.length === 0 ?
       ([
         <td>Not Available</td>,
       ])
       :
       ([
-        <td>{faculty.officeLocation.map((office) => <div>{office}</div>)}</td>,
+        <td>{faculty.officeLocation.map((office) => <div key={office}>{office}</div>)}</td>,
       ])}
     { user !== '' ?
       ([
